dd41fda5c621c05c1f18ad7c8514ff342d2a26cc
Paul Haddad <paulh16@gmail.com>
2016-01-10 21:07:44 -0600

    Implementation 2

--- nth_fibonacci/nth_fibonacci.rb
+++ nth_fibonacci/nth_fibonacci.rb
@@ -2,16 +2,25 @@ class NthFibonacci
   INDEX_0 = 0
   INDEX_1 = 1
 
-  def self.fib(index)
-    fibonacci_numbers = [INDEX_0, INDEX_1]
-    fibonacci_numbers[index]
-
-    if index > 1
-      2.upto(index) do |index|
-        fibonacci_numbers[index] = fibonacci_numbers[index - 2] + fibonacci_numbers[index - 1]
-      end
+  def self.fib(n)
+    if n < 0
+      raise ArgumentError, "Index cannot be negative"
     end
 
-    fibonacci_numbers[index]
+    if n == 0 || n == 1
+      return n
+    end
+
+    n_minus_2 = INDEX_0
+    n_minus_1 = INDEX_1
+    current = 0
+
+    2.upto(n) do |index|
+      current = n_minus_2 + n_minus_1
+      n_minus_2 = n_minus_1
+      n_minus_1 = current
+    end
+
+    current
   end
 end
